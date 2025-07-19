148874607daf873c4ade7fce630840abb93dff9f
FrodoB4884 <eliot.f.potts@gmail.com>
2025-07-19 15:58:55 +0100

    remove annoying things

--- routes/location.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-import services
-
-location_bp = Blueprint("location", __name__, url_prefix="/location")
-
-@location_bp.route("/test/<int:test>")
-def test(test):
-    data = test
-    return render_template("partials/test.html", data=data)
--- routes/orders.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-import services
-
-orders_bp = Blueprint("orders", __name__, url_prefix="/orders")
-
-@orders_bp.route("/test/<int:test>")
-def test(test):
-    data = test
-    return render_template("partials/test.html", data=data)
